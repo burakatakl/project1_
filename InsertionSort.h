0a04f611fddf3d89be186c50ecaf4b931f924f20
burakatakl <149960490+burakatakl@users.noreply.github.com>
2023-11-18 17:04:27 +0100

    here I created InsertionSort in header i gave the void

--- InsertionSort.h
+++ InsertionSort.h
@@ -1,8 +1,28 @@
 //
 // Created by BURAK on 18/11/2023.
-//
 
 #ifndef PROJECT1__INSERTIONSORT_H
 #define PROJECT1__INSERTIONSORT_H
+
+#include <iostream>
+
+void insertionSort( int arr[], int dimension ){
+    int elements;
+    int before;
+
+    for(int i=1;i<dimension; i++ ){
+        elements = arr[i];
+        before = i-1;
+
+        while(before>= 0 && arr[before]>elements){
+            arr[before+1]=arr[before];
+            before--;
+        }
+        arr[before+1]=elements;
+    }
+}
+
+
+
 
 #endif //PROJECT1__INSERTIONSORT_H
