85547b6a94fee482dcf19b5d27caaec50854a04e
burakatakl <149960490+burakatakl@users.noreply.github.com>
2023-11-26 21:40:38 +0100

    in my main.cpp i called the heapsort and insertionsort and I tested

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include "HeapSort.h"
+#include "InsertionSort.h"
+#include <vector>
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
+    std::vector<int> IS={12,3,5,6,7,8,9,22};
+    insertionSort(IS);
+    std::cout<<" InsertionSort: ";
+    for (auto it:IS){
+        std::cout<<it<<"\n";
+    }
+    std::cout<<"heapsort: "
+    std::vector HP = {1,5,7,2,8};
+    heapsort(HP, HP.size());
+    for(auto it : HP)
+        std::cout << it << "\n";
+
+
+
+
+
     return 0;
 }
