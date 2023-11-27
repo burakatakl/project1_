10d4bf3f11503e854a6d12c50e2a5639376ea1a6
burakatakl <149960490+burakatakl@users.noreply.github.com>
2023-11-27 20:35:54 +0100

    i create my quicksort function i expalin every part on this code I try to doing simple way is little bit hard :)

--- quicksort.h
+++ quicksort.h
@@ -2,12 +2,12 @@
 #define PROJECT1__QUICKSORT_H
 #include<iostream>
 #include <vector>
-//fiest i started to create divide operitor I am gonna crash just one area and its gonna put the right way
+//first i started to create divide operitor I am gonna crash just one area and middle one gonna put the right way
 int divide(std::vector<int>&arr,int lowest, int highest){
     int middle=arr[highest];
     int i=lowest-1;//index of smaller one
     for(int w=lowest;w<=highest-1;w++){
-        if(arr[w]<middle){
+        if(arr[w]<middle){//if this element is smaller one i am usung swap
             i++;
             std::swap(arr[i],arr[w]);
         }
@@ -15,7 +15,13 @@ int divide(std::vector<int>&arr,int lowest, int highest){
     std::swap(arr[i+1],arr[highest]);
     return(i+1);
 }
-
+void quickSort(std::vector<int>&arr, int lowest,int highest){
+    if(lowest<highest){
+        int middleIndex = divide(arr,lowest,highest);//i am taking to middle index .i am using to divade this vector
+        quickSort(arr,lowest,middleIndex-1);//first part
+        quickSort(arr,middleIndex+1,highest);// second part
+    }
+}
 
 
 
