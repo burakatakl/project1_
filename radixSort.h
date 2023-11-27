4026bef5d8a2731c5d34b2b06d37dfec3138627c
burakatakl <149960490+burakatakl@users.noreply.github.com>
2023-11-27 21:11:26 +0100

    i create radixSort function. and i write to help me with find to maximum element int this vector

---
+++ radixSort.h
@@ -0,0 +1,12 @@
+#ifndef PROJECT1__RADIXSORT_H
+#define PROJECT1__RADIXSORT_H
+#include <iostream>
+#include <vector>
+
+void radixSort(std::vector<int>&arr){
+    int maxElement=*std::max_element(arr.begin(),arr.end());// here i am find maxuim element
+
+}
+
+
+#endif //PROJECT1__RADIXSORT_H
