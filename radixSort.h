88b55c3dec93fb1a216b6f6d3d1538684e6595d2
burakatakl <149960490+burakatakl@users.noreply.github.com>
2023-11-27 21:53:50 +0100

    i created output string

--- radixSort.h
+++ radixSort.h
@@ -13,6 +13,11 @@ void radixSort(std::vector<int>&arr){
             count[(arr[i]/tt)%10]++; //here i am counting for every elements, elements
         for(int i=1; i<10;i++)
             count[i]+=count[i-1];// i am update count lines
+        for(int i=x-1;i>=0;i--){
+            output[count[(arr[i]/tt)%10]-1]=arr[i];
+            count[(arr[i]/tt)%10]--; // i create output string
+        }
+
 
     }
 
