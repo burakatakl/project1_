87b1bdd8f758c20004c27ee799c915acf25c0056
burakatakl <149960490+burakatakl@users.noreply.github.com>
2023-11-27 20:41:04 +0100

    i called my quicksort function i create vector and and tested

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "HeapSort.h"
 #include "InsertionSort.h"
 #include <vector>
+#include "quicksort.h"
 
 
 int main() {
@@ -9,15 +10,23 @@ int main() {
 
     std::vector<int> IS={12,3,5,6,7,8,9,22};
     insertionSort(IS);
-    std::cout<<" InsertionSort: ";
+    std::cout<<" InsertionSort: "<<std::endl;
     for (auto it:IS){
         std::cout<<it<<"\n";
     }
-    std::cout<<"heapsort: "
+    std::cout<<"heapsort: "<<std::endl;
     std::vector HP = {1,5,7,2,8};
     heapsort(HP, HP.size());
     for(auto it : HP)
         std::cout << it << "\n";
+
+    std::vector<int> QS={22,11,55,12,1,3,4,88};
+    int size=QS.size();
+    quickSort(QS,0,size-1);
+    std::cout<<"QuickSort:"<<std::endl;
+    for(auto it:QS){
+        std::cout<<it<<"\n";
+    }
 
 
 
