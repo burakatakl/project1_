f2817fb32347fe7e35e9e7ebe4e7069c90f96044
burakatakl <149960490+burakatakl@users.noreply.github.com>
2023-11-27 21:33:55 +0100

    i write some  methods

--- radixSort.h
+++ radixSort.h
@@ -6,6 +6,16 @@
 void radixSort(std::vector<int>&arr){
     int maxElement=*std::max_element(arr.begin(),arr.end());// here i am find maxuim element
 
+    for(int tt=1; maxElement/tt>0; tt*=10){
+        int x =arr.size();
+        std::vector<int> output(x),count(10,0);
+        for(int i=0; i<x; i++)
+            count[(arr[i]/tt)%10]++; //here i am counting for every elements, elements
+        for(int i=1; i<10;i++)
+            count[i]+=count[i-1];// i am update count lines
+
+    }
+
 }
 
 
